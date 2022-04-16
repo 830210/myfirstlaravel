aabe464874f8ded848f633a8d302821832cf7b23
830210 <49450286+830210@users.noreply.github.com>
2022-04-16 22:32:10 +0800

    go

---
+++ app/Http/Controllers/NewController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Requests;
+
+class NewController extends Controller
+{
+    public function index(){
+        return view('bootstrap');
+    }
+}
